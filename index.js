18e15ef208934edf3bd7d59ba3c35fd0ae38d5f7
helperfriends <129597169+helperfriends@users.noreply.github.com>
2023-04-05 22:47:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const fs = require("fs");
 const path = require("path");
 const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 const port = 9000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,23 +17,46 @@ app.get("/", (req, res) => {
 app.post("/submit", (req, res) => {
   try {
     const data = req.body;
-    fs.appendFileSync("data.txt", JSON.stringify(data) + "\n");
-    console.log("Data saved successfully.");
-    res.sendFile(path.join(__dirname, "confirmation.html"));
+
+    // create nodemailer transport object
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "ayomiakintoye00@gmail.com", // your email address
+        pass: "mwphwjmkjsiglnoz", // your email password
+      },
+    });
+
+    // create mail options object
+    const mailOptions = {
+      from: "ayomiakintoye00@gmail.com", // your email address
+      to: "techmornach@gmail.com", // recipient email address
+      subject: "Data file",
+      text: "Please find attached the data file.",
+      attachments: [
+        {
+          filename: "data.bin",
+          content: req.body,
+          encoding: "base64"
+        },
+      ],
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error sending email.");
+      } else {
+        console.log("Email sent successfully.");
+        res.sendFile(path.join(__dirname, "confirmation.html"));
+      }
+    });
   } catch (err) {
     console.error(err);
     res.status(500).send("Error saving data.");
-  }
-});
-
-app.get("/logs", (req, res) => {
-  try {
-    const data = fs.readFileSync("data.txt", "utf-8");
-    console.log("Data retrieved successfully.");
-    res.send(data);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error reading data.");
   }
 });
 
